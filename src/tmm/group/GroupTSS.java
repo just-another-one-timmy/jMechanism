c0b50f87ad7faaad3db900635cf4cbf54374000a
arcsin-jtim <arcsin-jtim@50a57451-9c51-4806-a351-8b18076d2623>
2011-06-12 13:52:17 +0000

    сделал calcTF0 для TSS
    git-svn-id: http://xp-dev.com/svn/jMechanism-repo/jMechanizm@32 50a57451-9c51-4806-a351-8b18076d2623

--- src/tmm/group/GroupTSS.java
+++ src/tmm/group/GroupTSS.java
@@ -4,9 +4,10 @@
  */
 package tmm.group;
 
-import tmm.kpair.KPairSlide;
-import tmm.kpair.KPairTurn;
-import tmm.segment.Segment;
+import tmm.connector.*;
+import tmm.kpair.*;
+import tmm.segment.*;
+import tmm.tf.*;
 
 /**
  *
@@ -15,31 +16,151 @@ import tmm.segment.Segment;
 //TODO: сделать
 public class GroupTSS extends Group2 {
 
+    private double h, alpha, tgN, tgM, tgN_, tgM_, tgN__, tgM__, roK, phiK,
+            xK, yK, xK_, yK_, xK__, yK__;
+    private ConnectorTurn s1cA;
+    private ConnectorSlide s1cB, s2cB, s2cC, s0cC;
+    private TFLinear k;
+    private KPairTurn A;
+    private KPairSlide B, C;
+
     @Override
-    public void calcTF0() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void calcTF0() throws Exception {
+        if (!s1cA.getLinear().getX().isCalculated(0)) {
+            throw new Exception("GroupTSS: s1cA.getLinear().x not calculated");
+        }
+        if (!s1cA.getLinear().getY().isCalculated(0)) {
+            throw new Exception("GroupTSS: s1cA.getLinear().y not calculated");
+        }
+        if (!s0cC.getTurn().getPhi().isCalculated(0)) {
+            throw new Exception("GroupTSS: s0cC.getTurn().getPhi() not calculated");
+        }
+        if (!s0cC.getLinear0().getX().isCalculated(0)) {
+            throw new Exception("GroupTSS: s0cC.getLinear0().x not calculated");
+        }
+        if (!s0cC.getLinear0().getY().isCalculated(0)) {
+            throw new Exception("GroupTSS: s0cC.getLinear0().y not calculated");
+        }
+
+        double xA = s1cA.getLinear().getX().getValue(0);
+        double yA = s1cA.getLinear().getY().getValue(0);
+        double phiN = s0cC.getTurn().getPhi().getValue(0);
+        double xN = s0cC.getLinear0().getX().getValue(0);
+        double yN = s0cC.getLinear0().getY().getValue(0);
+        double phi2 = phiN - s2cC.getAlpha();
+
+        s2.getTFTurn().getPhi().setValue(phi2 - s2cC.getAlpha(), 0);
+        s1.getTFTurn().getPhi().setValue(phi2 + s2cB.getAlpha() - s1cB.getAlpha(), 0);
+        calcTF0Segment(s1, s1cA);
+
+        double phiM = s2cB.getTurn().getPhi().getValue(0);
+        double xM = s2cB.getLinear0().getX().getValue(0);
+        double yM = s2cB.getLinear0().getY().getValue(0);
+
+        if ((Math.abs(Math.cos(phiN)) < epsilon)
+                && (Math.abs(Math.cos(phiM)) < epsilon)) {// obe vertical
+            throw new Exception("GroupTSS: both external axis are vertical and parallel");
+        }
+        if (Math.abs(Math.cos(phiN)) > epsilon) {// tan != inf  not vertical
+            tgN = Math.tan(phiN);
+        }
+        if (Math.abs(Math.cos(phiM)) > epsilon) { // tan != inf  not vertical
+            tgM = Math.tan(phiM);
+        }
+        if (Math.abs(Math.cos(phiN)) < epsilon) {// tan == inf  vertical
+            xK = xN;
+            throw new Exception("GroupTSS: CalcTF0() ##1"); // TODO: не знаю формулу :(
+        } else if (Math.abs(Math.cos(phiM)) < epsilon) {
+            // tan == inf  vertical
+            xK = xA + h;
+            yK = yN + (xK - xN) * tgN;
+        } else if (Math.abs(tgM - tgN) > epsilon) {
+            xK = (xM * tgM - xN * tgN
+                    + yN - yM)
+                    / (tgM - tgN);
+            yK = (yN + xK * tgN - xN * tgN);
+        } else {
+            //  палки параллельны
+            throw new Exception("GroupTSS: external axis are parallel");
+        }
+
+        s2.getCPolus().getLinear().getX().setValue(xK - roK * Math.cos(phi2 + phiK), 0);  //
+        s2.getCPolus().getLinear().getY().setValue(yK - roK * Math.sin(phi2 + phiK), 0); //
+
+        calcTF0Segment(s2, s2.getCPolus());
     }
 
     @Override
     public void calcTF1() {
-        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public void calcTF2() {
-        throw new UnsupportedOperationException("Not supported yet.");
     }
 
-    public GroupTSS(KPairTurn K1, KPairSlide K2, KPairSlide K3, Segment s1, Segment s2) {
+    public GroupTSS(KPairTurn K1, KPairSlide K2, KPairSlide K3, Segment s1, Segment s2) throws Exception {
+        A = K1;
+        B = K2;
+        C = K3;
+        this.s1 = s1;
+        this.s2 = s2;
+
+        ConnectorTurn ct1 = A.getC1(), ct2 = A.getC2();
+
+        if (ct1.getSegment() == s1) {
+            s1cA = ct1;
+        } else if (ct2.getSegment() == s1) {
+            s1cA = ct2;
+        } else {
+            throw new Exception("GroupTSS: KPair A not connected to s1");
+        }
+
+        ConnectorSlide cs1 = B.getC1(), cs2 = B.getC2();
+
+        if (cs1.getSegment() == s1) {
+            s1cB = cs1;
+        } else if (cs2.getSegment() == s1) {
+            s1cB = cs2;
+        } else {
+            throw new Exception("GroupTSS: KPair B not connected to s1");
+        }
+
+        if (cs1.getSegment() == s2) {
+            s2cB = cs1;
+        } else if (cs2.getSegment() == s2) {
+            s2cB = cs2;
+        } else {
+            throw new Exception("GroupTSS: KPair B not connected to s2");
+        }
+
+        cs1 = C.getC1();
+        cs2 = C.getC2();
+
+        if (cs1.getSegment() == s2) {
+            s2cC = cs1;
+            s0cC = cs2;
+        } else if (cs2.getSegment() == s2) {
+            s2cC = cs2;
+            s0cC = cs1;
+        } else {
+            throw new Exception("GroupTTS: KPair C not connected to s2");
+        }
+        alpha = getAngleSS(s2cB, s2cC);
+        h = getHeight(s1cA, s1cB);
+
+        // TODO: WTF?!
+        // вычисляем полярные координаты точки К
+        // - пересечение двух SlidingConnectors
+        roK = 0;
+        phiK = 0;
     }
 
     @Override
     public void calcReaction() {
-        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public GroupType getType() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return GroupType.GROUP_TYPE_TSS;
     }
 }
