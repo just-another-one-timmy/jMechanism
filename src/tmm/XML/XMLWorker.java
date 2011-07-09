75adfd4c42b28caf77a395016abcc4ded15ff92b
arcsin-jtim <arcsin-jtim@50a57451-9c51-4806-a351-8b18076d2623>
2011-07-09 20:55:15 +0000

    поправил XMLWorker, чтобы он мог загружать Group2
    git-svn-id: http://xp-dev.com/svn/jMechanism-repo/jMechanizm@46 50a57451-9c51-4806-a351-8b18076d2623

--- src/tmm/XML/XMLWorker.java
+++ src/tmm/XML/XMLWorker.java
@@ -5,6 +5,8 @@
 package tmm.XML;
 
 import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.xml.parsers.*;
 import org.xml.sax.*;
 import org.xml.sax.helpers.*;
@@ -16,6 +18,8 @@ import tmm.group.*;
  * @author jtimv
  */
 public class XMLWorker extends DefaultHandler {
+
+    private static final Logger logger  =  Logger.getLogger(XMLWorker.class.getName());
     
     private static final String ELEMENT_NAME_MECHANISM = "Mechanism",
             ELEMENT_NAME_SEGMENT = "Segment",
@@ -39,7 +43,10 @@ public class XMLWorker extends DefaultHandler {
             ATTR_NAME_ALPHA = "alpha",
             ATTR_NAME_C1_NAME = "c1_name",
             ATTR_NAME_C2_NAME = "c2_name",
-            ATTR_NAME_KPAIR_NAME = "kp_name",
+            ATTR_NAME_KPAIR_NAME = "kp_name", 
+            ATTR_NAME_KPAIR1_NAME = "kp1_name", 
+            ATTR_NAME_KPAIR2_NAME = "kp2_name", 
+            ATTR_NAME_KPAIR3_NAME = "kp3_name",
             ATTR_NAME_S1_NAME = "s1_name",
             ATTR_NAME_S2_NAME = "s2_name",
             ATTR_NAME_NEED_ANALYZE = "need_analyze";
@@ -47,20 +54,20 @@ public class XMLWorker extends DefaultHandler {
     GroupManager gm;
     String lastSegmentName;
     boolean needAnalyzeGroups;
-    
+
     public XMLWorker(CompManager cm, GroupManager gm) {
         this.cm = cm;
         this.gm = gm;
     }
-    
+
     @Override
     public void startDocument() {
     }
-    
+
     @Override
     public void endDocument() {
     }
-    
+
     @Override
     public void startElement(String uri, String name, String qName, Attributes atts) {
         if (ELEMENT_NAME_MECHANISM.equals(qName)) {
@@ -95,8 +102,16 @@ public class XMLWorker extends DefaultHandler {
             processElementGroup1(atts);
             return;
         }
+        if (ELEMENT_NAME_GROUP2.equals(qName)) {
+            try {
+                processElementGroup2(atts);
+            } catch (Exception ex) {
+                logger.log(Level.SEVERE, "Can't create Group2. Check your XML file.", ex);
+            }
+            return;
+        }
     }
-    
+
     @Override
     public void endElement(String uri, String name, String qName) {
         if (ELEMENT_NAME_GROUPS.equals(qName)) {
@@ -104,21 +119,21 @@ public class XMLWorker extends DefaultHandler {
             return;
         }
     }
-    
+
     public void loadFromFile(String filename) throws ParserConfigurationException, SAXException, IOException {
         cm.setBusy(true);
         SAXParserFactory.newInstance().newSAXParser().parse(filename, new XMLWorker(cm, gm));
         cm.createMissingTFs();
         cm.setBusy(false);
     }
-    
+
     private void processElementMechanism(Attributes atts) {
         gm.setStepSize(getV(ATTR_NAME_STEP_SIZE, atts));
         gm.setMinGC(getV(ATTR_NAME_MIN, atts));
         gm.setMaxGC(getDoubleValueEx(ATTR_NAME_MAX, atts, Double.MAX_VALUE));
         gm.setJump(getV(ATTR_NAME_JUMP, atts, false));
     }
-    
+
     private void processElementSegment(Attributes atts) {
         String name = getV(ATTR_NAME_NAME, atts, "");
         double mass = getV(ATTR_NAME_MASS, atts),
@@ -126,14 +141,14 @@ public class XMLWorker extends DefaultHandler {
         cm.addSegment(name, mass, rotInertia);
         lastSegmentName = name;
     }
-    
+
     private void processElementConnectorTurn(Attributes atts) {
         String name = getV(ATTR_NAME_NAME, atts, "");
         double ro = getV(ATTR_NAME_RO, atts),
                 phi = getV(ATTR_NAME_PHI, atts);
         cm.addConnectorTurn(name, lastSegmentName, ro, phi);
     }
-    
+
     private void processElementConnectorSlide(Attributes atts) {
         String name = getV(ATTR_NAME_NAME, atts, "");
         double ro = getV(ATTR_NAME_RO, atts),
@@ -141,10 +156,10 @@ public class XMLWorker extends DefaultHandler {
                 alpha = getV(ATTR_NAME_ALPHA, atts);
         cm.addConnectorSlide(name, lastSegmentName, ro, phi, alpha);
     }
-    
+
     private void processElementKPairs(Attributes atts) {
     }
-    
+
     private void processElementKPair(Attributes atts, String elementName) {
         String name = getV(ATTR_NAME_NAME, atts, ""),
                 c1_name = getV(ATTR_NAME_C1_NAME, atts, ""),
@@ -156,11 +171,11 @@ public class XMLWorker extends DefaultHandler {
             cm.addKPairSlide(name, c1_name, C2_name);
         }
     }
-    
+
     private void processElementGroupsStart(Attributes atts) {
         needAnalyzeGroups = getV(ATTR_NAME_NEED_ANALYZE, atts, false);
     }
-    
+
     private void processElementGroupsEnd() {
         if (needAnalyzeGroups) {
             try {
@@ -169,14 +184,22 @@ public class XMLWorker extends DefaultHandler {
             }
         }
     }
-    
+
     private void processElementGroup1(Attributes atts) {
         String kpairName = getV(ATTR_NAME_KPAIR_NAME, atts, ""),
                 s1Name = getV(ATTR_NAME_S1_NAME, atts, ""),
                 s2Name = getV(ATTR_NAME_S2_NAME, atts, "");
         gm.addFirstGroupByName(kpairName, s1Name, s2Name);
     }
-
+        
+    private void processElementGroup2(Attributes atts) throws Exception {
+        String kp1Name = getV(ATTR_NAME_KPAIR1_NAME, atts, ""),
+                kp2Name = getV(ATTR_NAME_KPAIR2_NAME, atts, ""),
+                kp3Name = getV(ATTR_NAME_KPAIR3_NAME, atts, ""),
+                s1Name = getV(ATTR_NAME_S1_NAME, atts, ""),
+                s2Name = getV(ATTR_NAME_S2_NAME, atts, "");
+        gm.addSecondGroupByName(kp1Name, kp2Name, kp3Name, s1Name, s2Name);
+    }
     //-----------------------------------------------------------------------
     // Helper functions. 
     //-----------------------------------------------------------------------
@@ -187,7 +210,7 @@ public class XMLWorker extends DefaultHandler {
         }
         return defaultValue;
     }
-    
+
     private String getStringValueEx(String attName, Attributes atts, String defaultValue) {
         String stringValue = atts.getValue(attName);
         if (stringValue != null) {
@@ -195,7 +218,7 @@ public class XMLWorker extends DefaultHandler {
         }
         return defaultValue;
     }
-    
+
     private boolean getBooleanValueEx(String attName, Attributes atts, boolean defaultValue) {
         String booleanValue = atts.getValue(attName);
         if (booleanValue != null) {
@@ -203,15 +226,15 @@ public class XMLWorker extends DefaultHandler {
         }
         return defaultValue;
     }
-    
+
     double getV(String attName, Attributes atts) {
         return getDoubleValueEx(attName, atts, 0.0);
     }
-    
+
     String getV(String attName, Attributes atts, String x) {
         return getStringValueEx(attName, atts, "");
     }
-    
+
     boolean getV(String attName, Attributes atts, boolean x) {
         return getBooleanValueEx(attName, atts, x);
     }
