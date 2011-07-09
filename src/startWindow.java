7afd728b55338cfbe5989bdce80bd048fb775630
arcsin-jtim <arcsin-jtim@50a57451-9c51-4806-a351-8b18076d2623>
2011-07-09 21:59:55 +0000

    T_TTT_TTT_simple менял параметры групп
    git-svn-id: http://xp-dev.com/svn/jMechanism-repo/jMechanizm@55 50a57451-9c51-4806-a351-8b18076d2623

--- src/startWindow.java
+++ src/startWindow.java
@@ -63,7 +63,7 @@ public final class startWindow extends JFrame {
     }
 
     public static void main(String[] args) throws Exception {
-        String[] names = {"T_TTT_TTS_simple"};
+        String[] names = {"T_TTT_TTT"};
         for (String x : names) {
             startWindow s = new startWindow(x);
             s.buildMechanizm(x);
