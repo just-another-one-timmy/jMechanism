6ed9207a6fb24ecfbb6df4740f18e1cd8980685a
Iaroslav Tymchenko <jtimchenko@gmail.com>
2011-09-04 22:03:33 +0100

    Implemented drawing of ground triangles; added new constants for colors and sizes

--- src/tmm/visual/ProcessingV.java
+++ src/tmm/visual/ProcessingV.java
@@ -5,14 +5,15 @@ import processing.core.*;
 public class ProcessingV extends Visualizer {
 
     private PApplet parent = null;
+    // scale
     private float sc = 250;
+    // translation
     private float tr = 200;
 
     public ProcessingV(PApplet parent) {
         this.parent = parent;
-        parent.fill(0);
-        parent.stroke(126);
         parent.smooth();
+        parent.colorMode(PApplet.RGB, 255);
     }
 
     @Override
@@ -23,5 +24,33 @@ public class ProcessingV extends Visualizer {
     @Override
     public void drawCircle(double x, double y, double radius) {
         parent.ellipse(tr + (float) x * sc, tr + (float) y * sc, 10, 10);
+    }
+
+    @Override
+    public void setColor(int color) {
+        parent.stroke((color & 0xFF0000) / 0xFFFF,
+                (color & 0x00FF00) / 0xFF,
+                color & 0x0000FF);
+    }
+
+    @Override
+    public void setBgColor(int bgColor) {
+        parent.fill((bgColor & 0xFF0000) / 0xFFFF,
+                (bgColor & 0x00FF00) / 0xFF,
+                bgColor & 0x0000FF);
+    }
+
+    @Override
+    public void drawPolygon(double[] points) throws Exception {
+        if (points.length % 2 != 0) {
+            throw new Exception("We are expecting even number of points for drawing polygon!");
+        }
+        parent.beginShape();
+        int i = 0;
+        while (i < points.length) {
+            parent.vertex((float) points[i] * sc + tr, (float) points[i + 1] * sc + tr);
+            i += 2;
+        }
+        parent.endShape(PApplet.CLOSE);
     }
 }
--- src/tmm/visual/Visualizer.java
+++ src/tmm/visual/Visualizer.java
@@ -10,7 +10,18 @@ public abstract class Visualizer {
     protected boolean drawSegments = true;
     protected boolean drawKPairs = true;
     protected CompManager cm = null;
-    private double R = 3, A = 10, B = 20, L = 40;
+    private double R = 3, A = 10, B = 20, L = 0.2;
+    protected double GROUND_TRIANGLE_X_DIFF = .075, GROUND_TRIANGLE_Y_DIFF = .1;
+    protected double GROUND_TRIANGLE_STRIKE_X_DIFF = .015,
+            GROUND_TRIANGLE_STRIKE_Y_DIFF = .025,
+            // MUST BE > 1 
+            GROUND_TRIANGLE_STRIKES_COUNT = 5;
+    protected static int COLOR_SEGMENT = 0x445566;
+    protected static int COLOR_KPAIR_FILL = 0x000010;
+    protected static int COLOR_KPAIR_STROKE = 0xFFFF00;
+    protected static int COLOR_GROUND_TRIANGLE_STROKE = 0x00FF00;
+    protected static int COLOR_GROUND_TRIANGLE_FILL = 0x005500;
+    protected static int COLOR_GROUND_TRIANGLE_STRIKES = 0x225500;
 
     public void draw() throws Exception {
         drawKPairs();
@@ -22,12 +33,14 @@ public abstract class Visualizer {
     void drawKPairs() throws Exception {
         for (KPair k : cm.getKPairs()) {
             if (!k.getC1().getSegment().getName().equals("Ground")) {
+                setColor(COLOR_SEGMENT);
                 drawLine(k.getLinear().getX().getValue(0),
                         k.getLinear().getY().getValue(0),
                         k.getC1().getSegment().getCPolus().getLinear().getX().getValue(0),
                         k.getC1().getSegment().getCPolus().getLinear().getY().getValue(0));
             }
             if (!k.getC2().getSegment().getName().equals("Ground")) {
+                setColor(COLOR_SEGMENT);
                 drawLine(k.getLinear().getX().getValue(0),
                         k.getLinear().getY().getValue(0),
                         k.getC2().getSegment().getCPolus().getLinear().getX().getValue(0),
@@ -46,7 +59,19 @@ public abstract class Visualizer {
         }
     }
 
-    private void drawGroundTurn(KPair k) {
+    private void drawGroundTurn(KPair k) throws Exception {
+        double xTop = k.getLinear().getX().getValue(0),
+                yTop = k.getLinear().getY().getValue(0);
+        double xRight = xTop + GROUND_TRIANGLE_X_DIFF, yRight = yTop + GROUND_TRIANGLE_Y_DIFF;
+        double xLeft = xTop - GROUND_TRIANGLE_X_DIFF, yLeft = yTop + GROUND_TRIANGLE_Y_DIFF;
+        setColor(COLOR_GROUND_TRIANGLE_STROKE);
+        setBgColor(COLOR_GROUND_TRIANGLE_FILL);
+        drawPolygon(new double[]{xTop, yTop, xLeft, yLeft, xRight, yRight});
+        double xStep = (xRight - xLeft) * 1.0 / (GROUND_TRIANGLE_STRIKES_COUNT - 1);
+        for (int i = 0; i < GROUND_TRIANGLE_STRIKES_COUNT; i++) {
+            drawLine(xLeft + i * xStep, yLeft, xLeft + i * xStep + GROUND_TRIANGLE_STRIKE_X_DIFF,
+                    yLeft + GROUND_TRIANGLE_STRIKE_Y_DIFF);
+        }
     }
 
     private void drawGroundSlideIn(KPair k) {
@@ -58,10 +83,12 @@ public abstract class Visualizer {
             // triangle
             drawGroundTurn(k);
         }
-        drawCircleForKPair(k);  // circle  
+        setColor(COLOR_KPAIR_STROKE);
+        setBgColor(COLOR_KPAIR_FILL);
+        drawCircleForKPair(k);
     }
 
-    private void drawKPairSlide(KPairSlide k) {
+    private void drawKPairSlide(KPairSlide k) throws Exception {
         if (k.getC1().getSegment().getName().equals("Ground")) {
             drawGroundSlideIn(k);
         } else {
@@ -78,8 +105,18 @@ public abstract class Visualizer {
         drawCircle(k.getLinear().getX().getValue(0), k.getLinear().getY().getValue(0), R);
     }
 
-    private void drawLinePol(KPairSlide k, TFTurn angle) {
-        //throw new UnsupportedOperationException("Not yet implemented");
+    private void drawLinePol(KPairSlide k, TFTurn angle) throws Exception {
+        /*
+        double phi = angle.getPhi().getValue(0);
+        double xCenter = k.getLinear().getX().getValue(0),
+        yCenter = k.getLinear().getY().getValue(0);
+        double xRight = k.getLinear().getX().getValue(0) + .5 * L * Math.cos(phi),
+        yRight = k.getLinear().getY().getValue(0) + .5 * L * Math.sin(phi);
+        double xLeft = k.getLinear().getX().getValue(0) - .5 * L * Math.cos(phi),
+        yLeft = k.getLinear().getY().getValue(0) + .5 * L * Math.sin(phi);
+        setColor(COLOR_SEGMENT);
+        drawLine(xLeft, yLeft, xRight, yRight);
+         */
     }
 
     private void drawRectPol(KPairSlide k, TFTurn angle, double A, double B) {
@@ -90,9 +127,15 @@ public abstract class Visualizer {
         //throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    public abstract void drawLine(double x1, double y1, double x2, double y2);
+    public abstract void drawLine(double x1, double y1, double x2, double y2) throws Exception;
 
-    public abstract void drawCircle(double x, double y, double radius);
+    public abstract void drawCircle(double x, double y, double radius) throws Exception;
+
+    public abstract void setColor(int color) throws Exception;
+
+    public abstract void setBgColor(int bgColor) throws Exception;
+
+    public abstract void drawPolygon(double[] points) throws Exception;
 
     public void setDebugMode(boolean debugMode) {
         this.debugMode = debugMode;
