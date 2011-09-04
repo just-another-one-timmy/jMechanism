74b721b137be6f09e22e5a556ecedd3fd63496fc
Iaroslav Tymchenko <jtimchenko@gmail.com>
2011-09-04 18:44:36 +0100

    we are using processing for visualization now! ^_^

--- src/tmm/visual/Visualizer.java
+++ src/tmm/visual/Visualizer.java
@@ -1,328 +1,124 @@
 package tmm.visual;
 
-import java.awt.Color;
-import java.util.logging.*;
 import tmm.compmanager.*;
-import tmm.connector.*;
 import tmm.kpair.*;
-import tmm.segment.*;
 import tmm.tf.*;
 
 public abstract class Visualizer {
 
-    protected boolean debugMode;
-    protected CompManager cm;
-    double scaleX = 1.0, scaleY, translateX = 0, translateY = 0;
-    double h;
-    private static final Logger logger = Logger.getLogger(Visualizer.class.getName());
-    //const R=3;a=10;b=20;l=40;
-    private static double R = 3, A = 10, B = 20, L = 40;
-    boolean useNewCode = false;
-
-    public void setUseNewCode(boolean x) {
-        useNewCode = x;
-    }
-
-    public Visualizer(CompManager c) {
-        cm = c;
-    }
-
-    private void drawConnectorTurn(ConnectorTurn ct, double pol_x, double pol_y, String sname, boolean drawText, String text) throws Exception {
-        if (!ct.getLinear().getX().isCalculated(0)) {
-            //    System.out.println("X not calcul " + ct.getName());
-        }
-        if (!ct.getLinear().getY().isCalculated(0)) {
-            //     System.out.println("Y not calcul " + ct.getName());
-        }
-        double x = ct.getLinear().getX().getValue(0);
-        double y = ct.getLinear().getY().getValue(0);
-        //System.out.println("Drawing " + ct.getName() + " at: " + x + "  " + y);
-        drawCircle(x, h - y, 5, 0xFFBB22, 2, 0xFFBB22);
-        if (!sname.equals("Ground")) {
-            drawLine(pol_x, h - pol_y, x, h - y, 2, 0xFFFFFF);
-        }
-        if (drawText) {
-            drawText(x, h - y, text);
-        }
-    }
-
-    private void drawConnectorSlide(ConnectorSlide cs, double pol_x, double pol_y, String sname, boolean drawText, String text) throws Exception {
-        double x = cs.getLinear0().getX().getValue(0);
-        double y = cs.getLinear0().getY().getValue(0);
-
-        logger.log(Level.FINE, "Drawing {0} at: {1}  {2}", new Object[]{cs.getName(), x, y});
-
-        double phi = cs.getTurn().getPhi().getValue(0);
-        double len = 10;
-        double delta_x = len * Math.cos(phi);
-        double delta_y = len * Math.sin(phi);
-        drawLine(x - delta_x, h - (y - delta_y), x + delta_x, h - (y + delta_y), 1, 0x00FF00);
-
-        double rh = .1, rw = .2;
-        double x1 = x - rw / 2, x2 = x + rw / 2;
-        double y1 = y + rh / 2, y2 = y - rh / 2;
-        //sf::Shape rect = sf::Shape::Rectangle(-rw/2,-rh/2,+rw/2,+rh/2,sf::Color::Blue, 2, sf::Color::Blue);
-        //rect.Rotate(phi*180/pi);
-        //rect.SetPosition(x, h-y);
-        //rect.EnableFill(false);
-        //w->Draw(rect);
-        //drawRect(x1, h - y, x2, y2);
-
-        drawSquare(x, h - y, 5, 0x00FF00);
-
-        if (!sname.equals("Ground")) {
-            drawLine(pol_x, h - pol_y, x, h - y, 1, 0x00BBFF);
-        }
-        if (drawText) {
-            drawText(x, h - y, text);
-        }
-    }
-
-    private void drawConnector(Connector c, double pol_x, double pol_y, String sname, boolean drawText, String text) throws Exception {
-        switch (c.getType()) {
-            case CONNECTOR_TYPE_TURN: {
-                drawConnectorTurn((ConnectorTurn) c, pol_x, pol_y, sname, drawText, text);
-                break;
-            }
-            case CONNECTOR_TYPE_SLIDE: {
-                drawConnectorSlide((ConnectorSlide) c, pol_x, pol_y, sname, drawText, text);
-                break;
-            }
-        }
-    }
-
-    private void drawKPair(KPair k) throws Exception {
-        Connector c = k.getC1();
-        Segment s = c.getSegment();
-        double pol_x = s.getCPolus().getLinear().getX().getValue(0);
-        double pol_y = s.getCPolus().getLinear().getY().getValue(0);
-        drawConnector(c, pol_x, pol_y, s.getName(), true, k.getName());
-    }
+    protected boolean debugMode = true;
+    protected boolean drawSegments = true;
+    protected boolean drawKPairs = true;
+    protected CompManager cm = null;
+    private double R = 3, A = 10, B = 20, L = 40;
 
     public void draw() throws Exception {
-        if (useNewCode) {
-            drawNew();
-            return;
-        }
-        if (cm.isBusy()) {
-            return;
-        }
-        for (Segment s : cm.getSegments()) {
-            double pol_x = s.getCPolus().getLinear().getX().getValue(0);
-            double pol_y = s.getCPolus().getLinear().getY().getValue(0);
-
-            for (Connector c : s.getConnectors()) {
-                try {
-                    drawConnector(c, pol_x, pol_y, s.getName(), false, "");
-                } catch (Exception e) {
-                    logger.log(Level.SEVERE, "Visualizer: problems with connector {0} : {1}", new String[]{c.getName(), e.getMessage()});
-                }
-            }
-        }
-        for (KPair k : cm.getKPairs()) {
-            drawKPair(k);
-        }
+        drawKPairs();
     }
 
-    public void drawNew() throws Exception {
-        if (!cm.isBusy()) {
-            drawSegments();
-            drawKPairs();
-        }
-    }
-
-    /* for(segments)
-     * if (segment!=ground)
-     *      line(CMass,polus);
+    /* Abstraction level = mechanism 
+     * for all methods below
      */
-    void drawSegments() throws Exception {
-        for (Segment s : cm.getSegments()) {
-            // Hm..? Doesn't look good for me. :(
-            if (!s.getName().equals("Ground")) {
-                drawLineBetweenConnectors(s.getCMass(), s.getCPolus());
-            }
-        }
-    }
-
-    /*
-     * for (kpairs)
-     * {
-     * if (kpair.c1.segment != ground)
-     *      line(kpair,kpair.c1.segment.polus);
-     * if (kpair.c2.segment != ground)
-    line(kpair,kpair.c2.segment.polus);
-     * switch(kpair.type)
-     *  {
-     *      case TURN :
-     *          if ((kpair.c1.segment == ground)
-     *          ||   kpair.c2.segment == ground))
-     *          drawGroundTurn(kpair);  // 
-     *          circle(kpair,R);  // кружок
-     *           
-     *      case SLIDE:
-     *      alfa = kpair.getAngle();
-     *      if (kpair.c1.segment == ground)
-     *              drawGroundSlideIn(kpair);    
-     *        else 
-     *              line_pol(kpair,alfa,l); // худая палка
-     *
-     *          if (kpair.c2.segment == ground)
-     *              drawGroundSlideOut(kpair);   
-     *         esle 
-     *              rect_pol(kpair,alfa,a,b);    // толстая палка
-     * 
-     *      }
-     * }
-     */
-    void drawKPairs() {
+    void drawKPairs() throws Exception {
         for (KPair k : cm.getKPairs()) {
             if (!k.getC1().getSegment().getName().equals("Ground")) {
-                //
+                drawLine(k.getLinear().getX().getValue(0),
+                        k.getLinear().getY().getValue(0),
+                        k.getC1().getSegment().getCPolus().getLinear().getX().getValue(0),
+                        k.getC1().getSegment().getCPolus().getLinear().getY().getValue(0));
             }
             if (!k.getC2().getSegment().getName().equals("Ground")) {
-                //
+                drawLine(k.getLinear().getX().getValue(0),
+                        k.getLinear().getY().getValue(0),
+                        k.getC2().getSegment().getCPolus().getLinear().getX().getValue(0),
+                        k.getC2().getSegment().getCPolus().getLinear().getY().getValue(0));
             }
             switch (k.getType()) {
                 case KPAIR_TYPE_TURN: {
-                    if (k.getC1().getSegment().getName().equals("Ground")
-                            || k.getC2().getSegment().getName().equals("Ground")) {
-                        drawGroundTurn(k); // triangle
-                    }
-                    circleForKPair(k, R);  // circle                    
+                    drawKPairTurn((KPairTurn) k);
                     break;
                 }
                 case KPAIR_TYPE_SLIDE: {
-                    if (k.getC1().getSegment().getName().equals("Ground")) {
-                        drawGroundSlideIn(k);
-                    } else {
-                        linePol(k, ((KPairSlide) k).getAngle(), L);
-                    }
-                    if (k.getC2().getSegment().getName().equals("Ground")) {
-                        drawGroundSlideIn(k);
-                    } else {
-                        rectPol(k, ((KPairSlide) k).getAngle(), A, B);
-                    }
+                    drawKPairSlide((KPairSlide) k);
                     break;
                 }
             }
         }
     }
 
-    public final void setDebugMode(boolean mode) {
-        debugMode = mode;
+    private void drawGroundTurn(KPair k) {
     }
 
-    public final boolean getDebugMode() {
+    private void drawGroundSlideIn(KPair k) {
+    }
+
+    private void drawKPairTurn(KPairTurn k) throws Exception {
+        if (k.getC1().getSegment().getName().equals("Ground")
+                || k.getC2().getSegment().getName().equals("Ground")) {
+            // triangle
+            drawGroundTurn(k);
+        }
+        drawCircleForKPair(k);  // circle  
+    }
+
+    private void drawKPairSlide(KPairSlide k) {
+        if (k.getC1().getSegment().getName().equals("Ground")) {
+            drawGroundSlideIn(k);
+        } else {
+            drawLinePol(k, ((KPairSlide) k).getAngle());
+        }
+        if (k.getC2().getSegment().getName().equals("Ground")) {
+            drawGroundSlideOut(k);
+        } else {
+            drawRectPol(k, ((KPairSlide) k).getAngle(), A, B);
+        }
+    }
+
+    private void drawCircleForKPair(KPairTurn k) throws Exception {
+        drawCircle(k.getLinear().getX().getValue(0), k.getLinear().getY().getValue(0), R);
+    }
+
+    private void drawLinePol(KPairSlide k, TFTurn angle) {
+        //throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    private void drawRectPol(KPairSlide k, TFTurn angle, double A, double B) {
+        //throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    private void drawGroundSlideOut(KPairSlide k) {
+        //throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public abstract void drawLine(double x1, double y1, double x2, double y2);
+
+    public abstract void drawCircle(double x, double y, double radius);
+
+    public void setDebugMode(boolean debugMode) {
+        this.debugMode = debugMode;
+    }
+
+    public void setDrawSegments(boolean drawSegments) {
+        this.drawSegments = drawSegments;
+    }
+
+    public void setDrawKPairs(boolean drawKPairs) {
+        this.drawKPairs = drawKPairs;
+    }
+
+    public boolean getDebugMode() {
         return debugMode;
     }
 
-    public final void setCompManager(CompManager c) {
-        cm = c;
+    public boolean getDrawSegments() {
+        return drawSegments;
     }
 
-    public final void setScalesAndTranslations(double scaleX, double scaleY, double translateX, double translateY) {
-        this.scaleX = scaleX;
-        this.scaleY = scaleY;
-        this.translateX = translateX;
-        this.translateY = translateY;
+    public boolean getDrawKPairs() {
+        return drawKPairs;
     }
 
-    void drawLinePol(double ro1, double phi1, double ro2, double phi2) {
-        //
-    }
-
-    void drawLinePolAngle(double ro, double phi, double alpha, double l) {
-        //
-    }
-
-    void drawGroundTurn(KPair k) {
-    }
-
-    void circleForKPair(KPair k, double R) {
-    }
-
-    void drawLineBetweenConnectors(Connector c1, Connector c2) throws Exception {
-        double x1 = 0, y1 = 0, x2 = 0, y2 = 0;
-        switch (c1.getType()) {
-            case CONNECTOR_TYPE_SLIDE: {
-                x1 = ((ConnectorSlide) c1).getLinear0().getX().getValue(0);
-                y1 = ((ConnectorSlide) c1).getLinear0().getY().getValue(0);
-                break;
-            }
-            case CONNECTOR_TYPE_TURN: {
-                x1 = ((ConnectorTurn) c1).getLinear().getX().getValue(0);
-                y1 = ((ConnectorTurn) c1).getLinear().getY().getValue(0);
-                break;
-            }
-        }
-        switch (c2.getType()) {
-            case CONNECTOR_TYPE_SLIDE: {
-                x2 = ((ConnectorSlide) c2).getLinear0().getX().getValue(0);
-                y2 = ((ConnectorSlide) c2).getLinear0().getY().getValue(0);
-                break;
-            }
-            case CONNECTOR_TYPE_TURN: {
-                x2 = ((ConnectorTurn) c2).getLinear().getX().getValue(0);
-                y2 = ((ConnectorTurn) c2).getLinear().getY().getValue(0);
-                break;
-            }
-        }
-        drawLine(x1, y1, x2, y2, 2, 0x00FF00);
-    }
-
-    void drawGroundSlideIn(KPair k) {
-    }
-
-    protected abstract void drawCircle(double x, double y, int radius, int color, int outline, int outlineColor);
-
-    protected abstract void drawLine(double x1, double y1, double x2, double y2, int thickness, int color);
-
-    protected abstract void drawText(double x, double y, String text);
-
-    protected abstract void drawSquare(double x, double y, int side, int color);
-
-    private void linePol(KPair k, TFTurn angle, double L) {
-//        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    private void rectPol(KPair k, TFTurn angle, double A, double B) {
-//       throw new UnsupportedOperationException("Not yet implemented");
+    public void setCompManager(CompManager cm) {
+        this.cm = cm;
     }
 }
-
-/*
- * 
- * 
- * line_pol рисует палку наклоненную на угол alfa, и проходящую через точку с координатами 
- * kpair
-circle and rect закрашивают область внутри себя.
- */
-
-/*  todo
- * еще на будущее надо предусмотреть возможность прорисовать 
- * одну группу или одно звено, или одну скорость/ускорение/силу . т.е. возможно нужно создать какие-то множества элементов
- * типа Visible. это для генератора отчетов. 
- * 
- */
-
-/* drawGroundTurn(kpair)
- * {
- * 
- *   o
- *  /_\
- * ////
- * }
-
- *drawGroundSlideIn(kpair);
- * {
- * ________________
- * ////
- * }
-drawGroundSlideOut(kpair);
- *{
- * ////////
- * --------
- * --------
- * ////////
- *}
- */
